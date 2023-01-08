ce90cf0047c15f2a7ea52fd8a17869bc8736165e
Darek <dariuszgrados@gmail.com>
2023-01-08 20:23:55 +0100

    wpis do rejestru

--- Program.cs
+++ Program.cs
@@ -44,9 +44,9 @@
 						}
 						else
 						{
-							AH = "00";
-							Console.WriteLine("Aktualny stan rejestru");
-							Console.WriteLine("AH: " + AH + " - niepoprawna wartość, przypisanie wartości 0");
+							AH = "00"; 
+							Console.WriteLine("Aktualny stan rejestru AH: " + AH);
+							Console.WriteLine("Wprowadzono niepoprawąa wartość, przypisano domyślną wartość");
 						}
 					}
 					else if (choice == "2" || choice == "BH")
@@ -70,13 +70,35 @@
 						else
 						{
 							BH = "00";
-							Console.WriteLine("Aktualny stan rejestru");
-							Console.WriteLine("BH: " + BH + " - niepoprawna wartość, przypisanie wartości 0");
+							Console.WriteLine("Aktualny stan rejestru BH: " + BH);
+							Console.WriteLine("Wprowadzono niepoprawąa wartość, przypisano domyślną wartość");
 						}
 
 					}
 					else if (choice == "3" || choice == "CH")
 					{
+						Console.Clear();
+						Console.WriteLine("Podaj wartoć rejestru CH");
+						string CH = Console.ReadLine();
+						CH = CH.ToLower();
+
+						if (CH.Length < 3
+							&& (CH[0] == 'a' || CH[0] == 'b' || CH[0] == 'c' || CH[0] == 'd' || CH[0] == 'e' || CH[0] == 'f'
+							 || CH[0] == '0' || CH[0] == '1' || CH[0] == '2' || CH[0] == '3' || CH[0] == '4' || CH[0] == '5'
+							 || CH[0] == '6' || CH[0] == '7' || CH[0] == '8' || CH[0] == '9')
+								
+							&& (CH[1] == 'a' || CH[1] == 'b' || CH[1] == 'c' || CH[1] == 'd' || CH[1] == 'e' || CH[1] == 'f'
+							 || CH[1] == '0' || CH[1] == '1' || CH[1] == '2' || CH[1] == '3' || CH[1] == '4' || CH[1] == '5'
+							 || CH[1] == '6' || CH[1] == '7' || CH[1] == '8' || CH[1] == '9'))
+						{
+							Console.WriteLine("Aktualny stan rejestru CH: " + CH);
+						}
+						else
+						{
+							CH = "00";
+							Console.WriteLine("Aktualny stan rejestru CH: " + CH);
+							Console.WriteLine("Wprowadzono niepoprawąa wartość, przypisano domyślną wartość");
+						}
 
 					}
 					else if (choice == "DH")
@@ -112,45 +134,6 @@
 			}
 
 		}
-
-		// wywwołanie funkcji żeby nasetępnie przypisać to i oszczędzić miejsce w kodzie - do sprawdzenia czy działa
-		public void Checker()
-		{
-			Console.WriteLine("Podaj wartoć rejestru AH");
-			string AH = Console.ReadLine();
-			AH = AH.ToLower();
-
-			if (AH.Length < 3
-				&& (AH[0] == 'a' || AH[0] == 'b' || AH[0] == 'c' || AH[0] == 'd' || AH[0] == 'e' || AH[0] == 'f'
-				 || AH[0] == '0' || AH[0] == '1' || AH[0] == '2' || AH[0] == '3' || AH[0] == '4' || AH[0] == '5'
-				 || AH[0] == '6' || AH[0] == '7' || AH[0] == '8' || AH[0] == '9')
-
-				&& (AH[1] == 'a' || AH[1] == 'b' || AH[1] == 'c' || AH[1] == 'd' || AH[1] == 'e' || AH[1] == 'f'
-				 || AH[1] == '0' || AH[1] == '1' || AH[1] == '2' || AH[1] == '3' || AH[1] == '4' || AH[1] == '5'
-				 || AH[1] == '6' || AH[1] == '7' || AH[1] == '8' || AH[1] == '9'))
-			{
-				Console.WriteLine("Aktualny stan rejestru AH: " + AH);
-			}
-			else
-			{
-				AH = "00";
-				Console.WriteLine("Aktualny stan rejestru");
-				Console.WriteLine("AH: " + AH + " - niepoprawna wartość, przypisanie wartości 0");
-			}
-		}
-		
-		//static void Menu()
-		//{
-		//	string option = "0";
-		//	do
-		//	{
-		//		Console.WriteLine("Wybierz numer aby wykonać odpowiednią akcję:");
-		//		Console.WriteLine("1) Wpisz wartość do rejestru\n2) Przenieś wartość z rejestru\n3) Zamień wartości w rejestrach\n4) Zakończ działanie symulacji");
-		//		option = Console.ReadLine();
-				
-
-		//	} while (option != "4");			
-		//}
 
 	}
 }
